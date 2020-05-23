1222321a37c1d5422d1757661bdc133d649d5dc8
JoaquinOnSoft <joaquingp@gmail.com>
2020-05-24 00:12:36 +0200

    New classes to use Solr API

---
+++ src/main/java/com/opentext/explore/connector/AbstractAPIWrapper.java
@@ -0,0 +1,95 @@
+package com.opentext.explore.connector;
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author Joaquín Garzón
+ */
+public abstract class AbstractAPIWrapper {
+
+	protected static final Log log = LogFactory.getLog(AbstractAPIWrapper.class);
+	protected String apiKey;
+
+	public AbstractAPIWrapper() {
+	}
+
+	/**
+	 * SEE: Apache HttpClient Examples
+	 * https://mkyong.com/java/apache-httpclient-examples/
+	 * @param request
+	 * @return
+	 */
+	protected String execute(HttpRequestBase request) {
+		String result = null;
+		CloseableHttpResponse response = null;
+
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+				
+		try {
+			response = httpClient.execute(request);
+
+			// Get HttpResponse Status
+			System.out.println(response.getProtocolVersion());              // HTTP/1.1
+			System.out.println(response.getStatusLine().getStatusCode());   // 200
+			System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+			System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+			HttpEntity entity = response.getEntity();
+			if (entity != null) {
+				// return it as a String
+				result = EntityUtils.toString(entity);
+				System.out.println(result);
+			}
+
+		} 
+		catch (IOException e) {
+			System.err.println(e.toString());
+		}
+		finally {
+			try {
+				if(response != null) {
+					response.close();
+				}
+				httpClient.close();
+			}
+			catch(IOException e) {
+				System.err.println(e.toString());				
+			}
+		}
+
+		return result;
+	}
+
+
+	protected Object jsonStringToObject(String json, Class<?> toClass) {
+		if(json == null) {
+			return null;	
+		}
+		else {		
+			// Jackson 2 – Convert Java Object to / from JSON
+			// https://www.mkyong.com/java/jackson-2-convert-java-object-to-from-json/
+			Object obj = null;
+
+			ObjectMapper mapper = new ObjectMapper();
+			//JSON string to Java Object
+			try {
+				obj = mapper.readValue(json.toString(), toClass);
+			} catch (IOException e) {
+				log.warn("API response (JSON Str to Object): " + e, e);
+				return null;
+			}
+			return obj;
+		}
+	}
+
+}
---
+++ src/main/java/com/opentext/explore/connector/SolrAPIWrapper.java
@@ -0,0 +1,58 @@
+package com.opentext.explore.connector;
+
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+
+public class SolrAPIWrapper extends AbstractAPIWrapper{
+	private String urlBase;
+	
+	private static final String METHOD_OTCA_BATCH_UPDATE = "otcaBatchUpdate";
+
+	public SolrAPIWrapper() {
+		this.urlBase = " http://localhost:8983/solr/interaction/";
+	}
+	
+	
+	public SolrAPIWrapper(String urlBase) {
+		this.urlBase = urlBase;
+	}
+	
+	/**
+	 * Below is the URL for the PostMan parameters.  
+	 * You should be able to paste this in and the Params will populate.  
+	 * Click on Body>binary and choose your file for import.  
+	 * 
+	 * http://localhost:8983/solr/interaction/otcaBatchUpdate?commitWithin=1000&otca.methods=languagedetector,diarisation,nsentiment,nsummarizer&otca=true&otca.diarisation.clientMapping=2&otca.diarisation.agentMapping=1
+
+	 * @return
+	 */
+	public String otcaBatchUpdate() {
+		String response = null;
+		URIBuilder builder = null;
+		
+		try {
+			builder = new URIBuilder(urlBase + METHOD_OTCA_BATCH_UPDATE);
+		} catch (URISyntaxException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		if(builder != null) {
+			builder.setParameter("commitWithin", "1000")
+				.setParameter("otca.methods", "languagedetector,diarisation,nsentiment,nsummarizer")
+				.setParameter("otca", "true")
+				.setParameter("otca.diarisation.clientMapping", "2")
+				.setParameter("otca.diarisation.agentMapping", "1");
+			
+			HttpGet request = new HttpGet();
+
+	        // add request headers
+	        request.addHeader(HttpHeaders.ACCEPT_CHARSET, "UTF-8");
+	        response = execute(request);			
+		}
+		
+		return response
+	}
+}
